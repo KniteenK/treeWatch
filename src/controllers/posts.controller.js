b7d532d69cfcd2ca51d102b1c500689dc895e69d
Niteen Kumar <niteen22336@iiitd.ac.in>
2025-03-21 19:15:13 +0530

    feat: implemented cloudinary

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,8 +1,16 @@
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import prisma from "../utils/prisma.js";
 
 const createPost = async (req, res) => {
     try {
-      const { title, content, image, authorId } = req.body;
+        const { title, content, authorId } = req.body;
+
+        const imageLocalPath = req.files?.image?.[0]?.path;
+        let imageUrl = null;
+        if (imageLocalPath) {
+        const uploadedImage = await uploadOnCloudinary(imageLocalPath);
+        imageUrl = uploadedImage;
+        }
 
         const user = await prisma.user.findUnique({ where: { id: authorId } });
 
@@ -10,51 +18,50 @@ const createPost = async (req, res) => {
             throw new Error("Unauthorized request");
         }
 
-      const post = await prisma.post.create({
-        data: { 
-            title, 
-            content, 
-            image, 
-            author : {connect: { id: user.id }} 
-        },
-      });
-      res.status(201).json(post);
+        const post = await prisma.post.create({
+            data: {
+                title,
+                content,
+                image: imageUrl,
+                author: { connect: { id: user.id } }
+            },
+        });
+        res.status(201).json(post);
     } catch (error) {
-      res.status(500).json({ message: "Error creating post", error });
+        res.status(500).json({ message: "Error creating post", error });
     }
-  };
-  
-  const getAllPosts = async (req, res) => {
+};
+
+const getAllPosts = async (_, res) => {
     try {
-      const posts = await prisma.post.findMany({ include: { author: true, comments: true } });
-      res.status(200).json(posts);
+        const posts = await prisma.post.findMany({ include: { author: true, comments: true } });
+        res.status(200).json(posts);
     } catch (error) {
         console.log(error.message);
-      res.status(500).json({ message: "Error fetching posts", error });
+        res.status(500).json({ message: "Error fetching posts", error });
     }
-  };
-  
-  const createComment = async (req, res) => {
+};
+
+const createComment = async (req, res) => {
     try {
-      const { content, postId, authorId } = req.body;
-      const comment = await prisma.comment.create({
-        data: { content, postId, authorId },
-      });
-      res.status(201).json(comment);
+        const { content, postId, authorId } = req.body;
+        const comment = await prisma.comment.create({
+            data: { content, postId, authorId },
+        });
+        res.status(201).json(comment);
     } catch (error) {
-      res.status(500).json({ message: "Error creating comment", error });
+        res.status(500).json({ message: "Error creating comment", error });
     }
-  };
-  
-  const getCommentsByPost = async (req, res) => {
+};
+
+const getCommentsByPost = async (req, res) => {
     try {
-      const { postId } = req.params;
-      const comments = await prisma.comment.findMany({ where: { postId }, include: { author: true } });
-      res.status(200).json(comments);
+        const { postId } = req.params;
+        const comments = await prisma.comment.findMany({ where: { postId }, include: { author: true } });
+        res.status(200).json(comments);
     } catch (error) {
-      res.status(500).json({ message: "Error fetching comments", error });
+        res.status(500).json({ message: "Error fetching comments", error });
     }
-  };
-  
-  export { createPost, getAllPosts, createComment, getCommentsByPost };
-  
+};
+
+export { createPost, getAllPosts, createComment, getCommentsByPost };
--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -3,7 +3,13 @@ import { Router } from "express";
 const router = new Router();
 
 import { createPost, getAllPosts, createComment, getCommentsByPost } from "../controllers/posts.controller.js" ;
-router.route ("/createPost").post (createPost) ;
+import { upload } from "../middlewares/multer.middleware.js";
+router.route ("/createPost").post (upload.fields([
+    {
+        name: "image",
+        maxCount: 1
+    }
+]) , createPost) ;
 router.route ("/getAllPosts").get (getAllPosts) ;
 router.route ("/createComment").post (createComment) ;
 router.route ("/getCommentsByPost/:postId").get (getCommentsByPost) ;
